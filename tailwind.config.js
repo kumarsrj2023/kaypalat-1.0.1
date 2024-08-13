833b3be875efbc8a5b712a25aa828b75cdfa4daf
Suraj kumar <kumarsrj2023@gmail.com>
2024-08-13 12:25:05 +0530

    best seller

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
     './templates/**/*.liquid',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-green": "#1AA565",
+        "custom-skyblue": "rgb(53, 164, 221)",
+      },
+      fontSize: {
+        "11": "11px",
+        "13": "13px",
+        "15": "15px",
+        "17": "17px",
+        "19": "19px",
+        "21": "21px",
+        "23": "23px",
+        "25": "25px",
+        "26": "26px",
+      }
+    },
   },
   plugins: [],
 }
